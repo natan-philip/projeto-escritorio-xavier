7782d93db0a0b494446a4cb54908490d9879ffed
natan-philip <natanphilip99@gmail.com>
2026-01-14 01:44:26 +0100

    ajustes finais

--- javascript/script.js
+++ javascript/script.js
@@ -7,12 +7,24 @@ const icon = document.querySelector('.btn-mobile i');
         icon.classList.toggle('fa-times');
         icon.classList.toggle('fa-bars');
 })
+
+const menuClose = document.querySelectorAll('.menu-item');
+
+    menuClose.forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.classList.remove('open');
+            document.body.style.overflow = 'auto';
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        })
+    })
+
 // SCROLL HEADER / adicionar sombra ao rolar a pagina   //
 window.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
+    const header = document.querySelector('.header');
     if (!header) return;
     if (window.scrollY > 0) {
-        header.style.boxShadow = '0px 4px 10px rgba(0, 0, 0, 0.15)';
+        header.style.boxShadow = '0px 4px 10px rgba(0, 0, 0, 0.35)';
     } else {
         header.style.boxShadow = 'none';
     }
