b10a613b832584b630120d115440588fe8b9dae8
natan-philip <natanphilip99@gmail.com>
2026-01-31 10:05:06 +0100

    fixacao de bugs de icones

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ const icon = document.querySelector('.btn-mobile i');
 
     btnMobile.addEventListener('click', () => {
         navLinks.classList.toggle('open');
-        icon.classList.toggle('fa-times');
+        icon.classList.toggle('fa-xmark');
         icon.classList.toggle('fa-bars');
 })
 
